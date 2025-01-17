b45da735dbbdafcd6856d6ba5a2f89dbadcac26b
SA10 <sa10@SA10s-MacBook-Air.local>
2025-01-16 20:13:02 -0500

    hello

--- weather_api.py
+++ weather_api.py
@@ -1,10 +1,27 @@
 import pandas as pd
-import requests
 import json
+import requests
 
 url = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-52.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure&timezone=auto"
-r = requests.get(url)
+response = requests.get(url)
 
-data = r.json()
+data = response.json()
 
-print(data)
+with open("data/json/url.json", mode = "w") as file:
+    json.dump(data, file, indent = 4)
+
+    df = pd.DataFrame(data)
+
+    # Extract the 'hourly' data from the fetched data
+hourly_data = data.get('hourly', {})
+# Prepare the data to be saved into a DataFrame
+json_data = {
+    "time": hourly_data.get("time", []),
+    "temperature_2m": hourly_data.get("temperature_2m", []),
+    "relative_humidity_2m": hourly_data.get("relative_humidity_2m", []),
+    "precipitation": hourly_data.get("precipitation", []),
+    "surface_pressure": hourly_data.get("surface_pressure", [])}
+
+df =pd.DataFrame(json_data)
+
+df.to_csv("data/csv/new.csv", index=False)
